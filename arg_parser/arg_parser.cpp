eff6bf54090dc99833d9dcccbc1b4928bf278c61
david-soto-m <davidsotomarchena@protonmail.com>
2021-01-28 01:06:16 +0100

    progres in arg_parser

--- arg_parser/arg_parser.cpp
+++ arg_parser/arg_parser.cpp
@@ -1,12 +1,31 @@
 #include "arg_parser.h"
 
-#include <iostream>
+#include <iostream> //momentarily
  
 Parser::Parser(int argc, char **argv){
-	std::string argv_s[argc-1];
-	for(int i=1;i<argc;i++){
-		argv_s[i-1]=std::string(argv[i]);
-		std::cout<<argv_s[i-1]<<"\n";
+	std::string argv_s;
+	if(argc<1){
+		doctype=diary;
+	}else{
+		for(int i=1;i<argc;i++){
+			argv_s=std::string(argv[i]);
+			if ((argv_s=="-s")|(argv_s=="--secret")){
+				doctype=secret;
+			}else if((argv_s=="-d")|(argv_s=="--diary")){
+				doctype=diary;
+			}else if((argv_s=="-t")|(argv_s=="--TODO")){
+				doctype=todo;
+			}else if((argv_s=="-n")|(argv_s=="--note")){
+				doctype=note;
+			}else if((argv_s=="-r")|(argv_s=="--read")){
+				is_dump=true;
+			}else{
+				doctype=diary;
+			}
+		}
+	}
+	if (is_dump && doctype==secret){
+		doctype=diary;
 	}
 }
 Parser::~Parser(){
--- arg_parser/arg_parser.h
+++ arg_parser/arg_parser.h
@@ -3,13 +3,14 @@
 	#include <string>
 # endif
 
-enum Docs_Case{diary,secret,dump,memo};
+typedef enum Docs_Case{diary,secret,note,todo} docs;
 
 class Parser{
 	private:
-		
+		bool is_dump=false;
+		docs doctype;
 	public:
 		Parser(int argc,char **argv);
 		~Parser();
-		Docs_Case get_case();
+		docs get_doctype();
 };
